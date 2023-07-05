f61a03ef55224bb8fdf941bba350426fd6aef090
Danilo <danilocbertolini@gmail.com>
2023-07-05 13:00:11 -0300

    .

--- src/components/Title/title.ts
+++ src/components/Title/title.ts
@@ -7,4 +7,7 @@ export const Title = styled.h1`
     color: ${(props) => props.theme.colors.primaryText };
     border-bottom: 1px solid transparent;
     text-shadow: 1px 1px 3px ${(props) => props.theme.colors.textShadow };
+    &:hover {
+        border-color: black;
+    }
 `
